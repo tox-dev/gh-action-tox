eff5ceec80f07e7f22c8bbcbff0c6690e2d8e5db
Sviatoslav Sydorenko <wk@sydorenko.org.ua>
2021-12-15 20:53:27 +0100

    Move log output to a dedicated module

--- src/inspect_tox_results.py
+++ src/inspect_tox_results.py
@@ -2,14 +2,18 @@
 
 import json
 import sys
+from pprint import pprint
 
-with open(sys.argv[1], 'r') as fd:
-    tox_results = json.load(fd)
-toxenv_data = tox_results['testenvs'][sys.argv[2]]
+print('::group::Printing json results...')
+
+results_json_string = sys.argv[1].strip().replace('\n', '%0A')
+toxenv_name = sys.argv[2].strip()
+
+tox_results = json.loads(results_json_string)
+toxenv_data = tox_results['testenvs'][toxenv_name]
 test_commands = toxenv_data['test']
 
 for test_cmd in test_commands:
-    from pprint import pprint
     pprint(test_cmd)
 
     rc = test_cmd['retcode']
@@ -24,8 +28,10 @@ for test_cmd in test_commands:
         )
         continue
 
+    test_cmd_out = test_cmd['output'].replace('\n', '%0A')
     print(
         '::debug:({rc}) $ {cmd}'.
         format(rc=rc, cmd=' '.join(test_cmd['command']))
     )
+    print(f'::debug:{test_cmd_out}')
     print('::debug:{out}'.format(out=test_cmd['output'].replace('\n', '%0A')))
