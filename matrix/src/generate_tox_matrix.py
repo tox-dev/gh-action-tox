3afd79306389779db2f2e693ae5244db316bbe49
Sviatoslav Sydorenko <wk@sydorenko.org.ua>
2021-12-18 02:06:02 +0100

    Move matrix generation to a Python module

---
+++ matrix/src/generate_tox_matrix.py
@@ -0,0 +1,62 @@
+import json
+import re
+import subprocess
+import sys
+
+
+print('::group::Generating GHA environments based on tox config')
+filter_pattern = sys.argv[1]
+
+min_py_ver, max_py_ver = sys.argv[2:3]
+if min_py_ver is not None:
+    min_py_ver = tuple(map(int, min_py_ver.split('.')))
+else:
+    min_py_ver = 2, 7
+if max_py_ver is not None:
+    max_py_ver = tuple(map(int, max_py_ver.split('.')))
+else:
+    max_py_ver = 3, 10
+
+def inc_minor_py_ver(ver):
+    if ver == (2, 7):
+        return 3, 5
+    return ver[0], ver[1] + 1
+
+def generate_py_vers(min_py, max_py):
+    cur_ver = min_py
+    while cur_ver <= max_py:
+        yield cur_ver
+        cur_ver = inc_minor_py_ver(cur_ver)
+
+tox_discovery_cmd = sys.executable, '-m', 'tox', '-a'
+toxenvs = set(subprocess.check_output(
+    tox_discovery_cmd,
+    universal_newlines=True,
+).splitlines())
+if {'py', 'python'} & toxenvs:
+    toxenvs -= {'py', 'python'}
+    for py_ver in generate_py_vers(min_py_ver, max_py_ver):
+        toxenvs |= {f'py{py_ver[0]}{py_ver[1]}'}
+
+envs = []
+for toxenv in toxenvs:
+    if filter_pattern and not re.search(filter_pattern, toxenv):
+        print(
+            f'`{toxenv}` does not march `{filter_pattern}`. '
+            'Excluding it...',
+        )
+        continue
+
+    print(f'Adding `{toxenv}` to the list')
+    py_ver = (
+        (toxenv[2], toxenv[3:]) if toxenv.startswith('py')
+        else max_py_ver
+    )
+    envs.append({
+        'python-version': '.'.join(py_ver),
+        'toxenv': toxenv,
+    })
+
+matrix_json_string = json.dumps({'include': envs})
+print(f'::set-output name=matrix::{matrix_json_string}')
+print('::endgroup::')
