27b0e10064a74a691bf304f2dfeb88257eb9f45d
Sviatoslav Sydorenko <wk@sydorenko.org.ua>
2021-12-18 15:22:57 +0100

    Recover Py2 compat in make tmp dir module

--- src/make_tmp_dir.py
+++ src/make_tmp_dir.py
@@ -1,25 +1,32 @@
 #! /usr/bin/env python
-import pathlib
+
+# IMPORTANT: This module runs under a variaty of Python versions and
+# IMPORTANT: must keep supporting Python 2.7+ syntax.
+
+import os
 import sys
 import tempfile
 
 
-gha_path = pathlib.Path(sys.argv[1]).absolute()
-tmp_root_dir = gha_path / '.tmp'
+gha_path = os.path.abspath(sys.argv[1])
+tmp_root_dir = os.path.join(gha_path, '.tmp')
 
 
-tmp_root_dir.mkdir()
+os.makedirs(tmp_root_dir)
 
 
 print('::group::Preparing tox results storage...')
 
 
-tmp_dir = pathlib.Path(tempfile.mkdtemp('-results', 'tox-', str(tmp_root_dir)))
-print(f'::set-output name=results-dir::{tmp_dir!s}')
+tmp_dir = tempfile.mkdtemp('-results', 'tox-', tmp_root_dir)
+print('::set-output name=results-dir::{path!s}'.format(path=tmp_dir))
 
 
-json_results_file_path = tmp_dir / '.tox-run-results.json'
-print(f'::set-output name=results-file::{json_results_file_path!s}')
+json_results_file_path = os.path.join(tmp_dir, '.tox-run-results.json')
+print(
+    '::set-output name=results-file::{path!s}'.
+    format(path=json_results_file_path),
+)
 
 
 print('::endgroup::')
