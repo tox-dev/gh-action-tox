66fdda1be2dff53ba051da331834673f16204555
Sviatoslav Sydorenko <wk@sydorenko.org.ua>
2021-12-19 17:21:31 +0100

    Urlencode the output contents

--- src/save_tox_results_as_output.py
+++ src/save_tox_results_as_output.py
@@ -10,6 +10,13 @@ results_file = Path(argv[1])
 tox_results = json.loads(results_file.read_text())
 compact_tox_results = json.dumps(tox_results, indent=None)
 
-print(f'::set-output name=json-results::{compact_tox_results}')
+urlencoded_tox_results = (
+    compact_tox_results.
+    replace('%', '%25').
+    replace('\r', '%0D').
+    replace('\n', '%0A')
+)
+
+print(f'::set-output name=json-results::{urlencoded_tox_results}')
 
 print('::endgroup::')
