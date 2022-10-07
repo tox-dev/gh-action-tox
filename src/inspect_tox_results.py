ea2050c79e561b17f1d28f37570735ca40df129a
Sviatoslav Sydorenko <wk@sydorenko.org.ua>
2022-10-07 03:26:21 +0200

    Add tox results to the job summary

--- src/inspect_tox_results.py
+++ src/inspect_tox_results.py
@@ -1,8 +1,13 @@
 #! /usr/bin/env python
 
 import json
+import os
 import sys
 from pprint import pprint
+
+
+FILE_APPEND_MODE = 'a'
+
 
 print('::group::Printing json results...')
 
@@ -10,31 +15,73 @@ results_json_string = sys.argv[1].strip().replace('\n', '%0A')
 toxenv_name = sys.argv[2].strip()
 tox_results_file = sys.argv[3]
 
+github_summary_file_path = os.environ['GITHUB_STEP_SUMMARY']
+
 tox_results = json.loads(results_json_string)
 with open(tox_results_file, 'r') as tox_results_fd:
     tox_results = json.load(tox_results_fd)
 toxenv_data = tox_results['testenvs'][toxenv_name]
 test_commands = toxenv_data['test']
 
-for test_cmd in test_commands:
-    pprint(test_cmd)
+with open(github_summary_file_path, mode=FILE_APPEND_MODE) as summary_file:
+    for test_cmd in test_commands:
+        pprint(test_cmd)
+        summary_file.write('# Tox run command result')
+        summary_file.write('\n')
+        summary_file.write(test_cmd)
 
-    rc = test_cmd['retcode']
-    if rc:
+        summary_file.write('\n')
+        summary_file.write('\n')
+
+        test_cmd_out = test_cmd['output']
+        escaped_test_cmd_out = test_cmd_out.replace('\n', '%0A')
+
+        rc = test_cmd['retcode']
+        if rc:
+            print(
+                '::error file=({rc}) $ {cmd}::{out}'.
+                format(
+                    rc=rc,
+                    cmd=' '.join(test_cmd['command']),
+                    out=escaped_test_cmd_out,
+                )
+            )
+
+            summary_file.write('```console')
+            summary_file.write('\n')
+            summary_file.write(
+                '({rc}) $ {cmd}\n{out}'.
+                format(
+                    rc=rc,
+                    cmd=' '.join(test_cmd['command']),
+                    out=test_cmd_out,
+                )
+            )
+            summary_file.write('\n')
+            summary_file.write('```')
+            summary_file.write('\n')
+            summary_file.write('\n')
+
+            continue
+
         print(
-            '::error file=({rc}) $ {cmd}::{out}'.
+            '::debug:({rc}) $ {cmd}'.
+            format(rc=rc, cmd=' '.join(test_cmd['command']))
+        )
+        print(f'::debug:{test_cmd_out}')
+        print('::debug:{out}'.format(out=escaped_test_cmd_out))
+
+        summary_file.write('```console')
+        summary_file.write('\n')
+        summary_file.write(
+            '({rc}) $ {cmd}\n{out}'.
             format(
                 rc=rc,
                 cmd=' '.join(test_cmd['command']),
-                out=test_cmd['output'].replace('\n', '%0A'),
+                out=test_cmd_out,
             )
         )
-        continue
-
-    test_cmd_out = test_cmd['output'].replace('\n', '%0A')
-    print(
-        '::debug:({rc}) $ {cmd}'.
-        format(rc=rc, cmd=' '.join(test_cmd['command']))
-    )
-    print(f'::debug:{test_cmd_out}')
-    print('::debug:{out}'.format(out=test_cmd['output'].replace('\n', '%0A')))
+        summary_file.write('\n')
+        summary_file.write('```')
+        summary_file.write('\n')
+        summary_file.write('\n')
